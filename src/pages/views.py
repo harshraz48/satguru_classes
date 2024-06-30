71194008022d9e3c01e906ca698f8c812299a495
Anjali Chauhan <anjalic9799@gmail.com>
2024-06-30 22:44:43 +0530

    frontend done

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, "pages/home.html")
 
+
 def about_us(request):
     return render(request, "pages/about_us.html")
 
+
 def results(request):
     return render(request, "pages/results.html")
+
 
 def courses(request):
     return render(request, "pages/courses.html")
